0192ee916f1748b47cdf5a475fcd3a231cdf1f2b
Luny Luny <lunylein@gmail.com>
2020-03-10 13:45:39 +0100

    Main Methode change

---
+++ Codewars/Chambouliard.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using System;
+using System.Text.RegularExpressions;
+
+namespace Codewars
+{
+    class Chambouliard
+    {
+        public static double Doubles(int maxk, int maxn)
+        {
+            double sum = 0;
+            for (int k = 1; k <= maxk; k++)
+            {
+                for (int n = 1; n <= maxn; n++)
+                {
+                    sum += v(k, n);
+                }
+            }
+            return sum;
+        }
+
+        public static double v(int k, int n)
+        {
+            return 1 / (k * Math.Pow(n + 1, 2 * k));
+        }
+        private static void assertFuzzyEquals(double act, double exp)
+        {
+            bool inrange = Math.Abs(act - exp) <= 1e-6;
+            if (inrange == false)
+            {
+                string specifier = "#0.000000";
+                Console.WriteLine(
+                    "At 1e-6: Expected must be " + exp.ToString(specifier) + ", but got " + act.ToString(specifier));
+            }
+            Console.WriteLine(inrange);
+        }
+        public static void Main2()
+        {
+            assertFuzzyEquals(Chambouliard.Doubles(1, 10), 0.5580321939764581); // 0.5580321939764581
+            assertFuzzyEquals(Chambouliard.Doubles(10, 1000), 0.6921486500921933); // 0.6921486500921933
+            assertFuzzyEquals(Chambouliard.Doubles(10, 10000), 0.6930471674194457); // 0.6930471674194457
+            assertFuzzyEquals(Chambouliard.Doubles(20, 10000), 0.6930471955575918); // 0.6930471955575918
+            Console.ReadKey();
+        }
+    }
+}
--- Codewars/Stripper.cs
+++ Codewars/Stripper.cs
@@ -23,7 +23,7 @@ namespace Codewars
             return text.Remove(text.Length - 1);
         }
 
-        public static void Main()
+        public static void Mai2n()
         {
             string result = StripComments("\n\n\nCEA\n\nAA\nAEDFFAFD\n\nD\n\n!\n\nF\n\n\n\n\nEA\n\n\n\n\n\nC\n\nA\n\nB!C\n\n\nB\n\nC\n\nCB\n\nE\n\nC\n\nCFFFE\n\n!\n\nE\n\nAAE\n\nBAE\n\nA!\n\nB\n\nA\n\n!\n\nBD\n\nBFEF\n\nE\n\nE\n\n!\n\nDAE\n\nFE\n\n\n\n!\n\nFD\n\nEB\n\nD\n\nB!A\nC\n\nB\n\n\n!!\n\nA\nB\n\nB\n\nFDF\n\n!\n\nA\n\nC\n\nCFCA\nEA\n\n\nC\n\nCB\n\n\nFA\n\n\nB\n\nA!\n\nA\n\nC\n\n!\n\nD\n\nBC!\n\nAF\n\nEBE\n\nC", new string[] { "#", "$", "!", "-" });
 
